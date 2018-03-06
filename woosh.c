ba54db7d8840c09fa82f04179744bc007c6af290
Anastasia Saverchenko <saverchenko.anastasia@gmail.com>
2018-03-06 09:58:43 -0500

    read and parse

--- woosh.c
+++ woosh.c
@@ -9,60 +9,103 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define MAX_ARGS 10
+#define LINE_LENGTH 128
 
 
-char * createListArguments(){
-	
-}
 
 void reportError() {
   char error_message[30] = "An error has occurred\n";
   write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
+void readLine(char line[]){
+	char* ret = fgets(line, LINE_LENGTH, stdin);
+
+	if(strcmp(line, "exit\n")==0 || ret == NULL){
+		exit(0);
+	}
+	int i =0;
+	while (line[i]!='\n'){
+		i++;
+	}
+	line[i] = '\0';
+}
+
+
+int parseLine(char* args[], char line[]){
+	int i =0;
+	args[i] = strtok(line, " ");
+	if (args[i] == NULL){
+		return 1;
+	}
+
+	while(args[i]!=NULL){
+		i++;
+		args[i] =strtok(NULL, " ");
+	}
+	return 1;
+}
+
+//copied from stack overflow, for now
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the null-terminator
+    //in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+
 int main(int argc, char **argv)
 {
-  int start = 1; 
-  //if start of program, no additional arguments
-  if (argc>1 && start){
-  	reportError();
-  	exit(1);
-  }
+    int start = 1; 
+    //if start of program, no additional arguments
+    if (argc>1 && start){
+  		reportError();
+  		exit(1);
+ 	}
+  
+  	//store and read the line
+  	char* args[MAX_ARGS];
+  	char line[LINE_LENGTH];
+  
 
-  char initialPath[] = "/bin";
+  	char path[] = "/bin/";
 
-  char *listArgs = createListArguments();
+  	pid_t pid = fork();
+  	pid_t wpid;
 
-  printf("woosh> ");
+  	while (1){
+  		printf("woosh> ");
+  		readLine(line);
+  		parseLine(args, line);
 
-  pid_t pid = fork();
+  		char* newPath = concat(path, args[0]);
 
-  int cont = 1;
-  while (cont){
-	//error
-	if (pid<0){
-	  reportError();
-	  exit(1);
-	}
+		//modified wait.c which was shown in class
+		pid_t pid;
+		pid = fork();
+		if (pid < 0) {
+	  		fprintf(stderr,"Error: can't fork a process\n");
+	  		perror("fork()");
+	  		exit(1);
+		} 
+		else if (pid) { // I am the parent
+	  		int status;
+	  		// wait for the child
+	  		if (waitpid(pid,&status,0) == -1) {
+	    	perror("waitpid()");
+	  		}
+		}
+		else {
 
-	//parent process
-	if (pid>0){
-	  pid_t waited1;
-      int status1;
-	  // waiting for a child
-      waited1 = wait(&status1);
-      printf("I am the parent");
-
-	}
-
-	//child process
-	else{
-	   printf("I am the child");
-	   cont = 0;
-
-	  //execv(initialPath, listArgs);
-	}
-
-  }
+	  	//fprintf(stdout,"I am the child and my pid is %d\n",getpid());
+	  	execv(newPath, args);
+		}
+		free(newPath);
+  	}
 
 }
+
